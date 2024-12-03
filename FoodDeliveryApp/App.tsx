e96939ee2a4c3d2454f5973415279c2bdf37a861
KyeongYeongDEV <yeong7098@naver.com>
2024-12-03 11:51:09 +0900

    FEAT : 로그임화면 추가

--- FoodDeliveryApp/App.tsx
+++ FoodDeliveryApp/App.tsx
@@ -1,113 +1,69 @@
 import * as React from 'react';
-import {NavigationContainer } from '@react-navigation/native';
-import {
-  createNativeStackNavigator,
-  NativeStackScreenProps,
-} from '@react-navigation/native-stack';
-import {Pressable, Text, TouchableHighlight, View} from 'react-native';
-import {useCallback} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Settings from './src/pages/Settings';
+import Orders from './src/pages/Orders';
+import Delivery from './src/pages/Delivery';
+import {useState} from 'react';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
-type RootStackParamList = {
-  Home: undefined; // Home 은 파라미터 없음 | 있다면 추가해줘야 함
-  Details: undefined;
+// 어떤 페이지가 있을지 개발 초기에 미리 다 정해주는 것이 좋다
+export type LoggedInParamList = {
+  Orders: undefined; // 주문화면
+  Settings: undefined; // 정산 화면
+  Delivery: undefined; // 배달 길 안내
+  Complete: {orderId: string}; // 고객에게 완료 됐다고 알림 | 사진을 찍어서 할 거임 | 주문마다 고유한 아이디를 파라미터로 넘겨줄 거임
+};
+// 화면에 나타내는 조건이 달라서 나눠놨음
+//
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
 };
 
-// HomeScreen과 DetailsScreen의 Props 타입
-// 공식 문서를 참고해서 type을 지정해야 한다.
-// React navigate 공식문서 보기 -> 사이트에 들엇가서 type checking list 검색
-// 강의 제목 '리액ㅌ느 내비게이션 화면 전환하기' 참고
-//  https://reactnavigation.org/docs/typescript/      | 공식 사이트
-type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
-type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
-
-function HomeScreen({navigation}: HomeScreenProps) {
-  const onClick = useCallback(() => {
-    // 아래의 코드를 이용해 버튼이 눌렸을 때 Details로 가도록 설정되었다.
-    navigation.navigate('Details');
-  }, [navigation]);
-  // flex = 서로간의 차지하는 비율, 1일 경우 전부를 뜻한다.
-  //코딩을 할 때 backgroundColor를 주는 것이 좋다
-  //flex 같은 함수 안에 있을 때 전체를 더한 것 분의 해당하는 수만큼 나눠 가짐 Ex) 1과 2 이렇게 두개 가 잇으면 1/3, 2/3나눠가짐
-  // justifyContent: 'center' 세로 중앙 정렬 / flex-end = 젤 밑에 정렬 / flex-start = 젤 위에 정렬
-  // alignItems: 'center' 가로 가운데 정렬 나머지도 위와 동일
-  // TouchableHighlight 버튼과 동일 = 화면에 글씨 꾹 누르면 됨 Touchable 종류가 많음
-  // 전체를 View로 묶고 flexDirection : 'row'로 주면 가로로 설정된다.
-  // flexDirection 얘에 따라서 alignItems, justifyContent 얘네들의 역할이 서로 달라진다.
-
-  return (
-    <View style={{flexDirection : 'row'}}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'yellow',
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-        }}>
-        <Pressable
-          onPress={onClick}
-          style={{
-            paddingTop: 20,
-            paddingBottom: 20,
-            paddingLeft: 40,
-            paddingRight: 40,
-            backgroundColor: 'blue',
-          }}>
-          <Text style={{color: 'white'}}>Home Screen</Text>
-        </Pressable>
-      </View>
-      <View style={{flex: 2, backgroundColor: 'orange'}}>
-        {' '}
-        <Text>Second</Text>{' '}
-      </View>
-    </View>
-  );
-}
-
-function DetailsScreen({navigation}: DetailsScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Home');
-  }, [navigation]);
-
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <TouchableHighlight onPress={onClick}>
-        <Text>Details Screen</Text>
-      </TouchableHighlight>
-    </View>
-  );
-}
-
-// Stack.Navigator initialRouteName="Home" 현재스크린이 두 개가 있는데 누구를 기본으로 둘 거냐
-// 코딩을 시작할 때 어떤 네비게이터들이 필요한지 생각을 하고 미리 깔아놔라
-// Stack으로 네비게이션 인스턴스를 만들고 불러오면 네비게이션을 만든다. 내부는 아래와 같음
-// function Stack() {
-//   return <View></View>
-// }
-//
-// function Navigator() {
-//   return <View></View>
-// }
-//
-// function Screen {
-//   return <View> </View>
-// }
-//
-// Stack.Navigator = Navigator;
-// Stack.Navigator =Screen;
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen} // component를 이용해 HomeScreen 을 이용해 네비게이션과 라우트라는 props를 전달해준다.
-          options={{title: '홈화면'}}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Group>
+            <Tab.Screen
+              name="Delivery"
+              component={Delivery}
+              options={{headerShown: false}}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={Settings}
+              options={{title: '내 정보'}}
+            />
+          </Tab.Group>
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
---
+++ FoodDeliveryApp/src/pages/SignIn.tsx
@@ -0,0 +1,95 @@
+import React, {useCallback, useState} from 'react';
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEamil = useCallback(text => {
+    setEmail(text);
+  }, []);
+  const onChangePassward = useCallback(text => {
+    setPassword(text);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    Alert.alert('알림', '안녕~');
+  }, []);
+  const canGoNext = email && password;
+
+  return (
+    <View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이메일을 입력해주세요."
+          onChangeText={onChangeEamil}
+        />
+      </View>
+      <View>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호를 입력해주세요."
+          onChangeText={onChangePassward}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          onPress={onSubmit}
+          style={
+            !canGoNext
+              ? styles.loginButton
+              : [styles.loginButton, styles.loginButtonActive]
+          }
+          disabled={!canGoNext}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable onPress={onSubmit} style={styles.loginButton}>
+          <Text>회원가입하기</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    padding: 20,
+  },
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  loginButtonActive: {
+    backgroundColor: 'blue',
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  buttonZone: {
+    alignItems: 'center',
+  },
+});
+
+export default SignIn;
