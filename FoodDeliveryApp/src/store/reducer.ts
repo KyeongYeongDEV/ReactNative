181c462bb457955aed971e26667add7bcb161eae
KyeongYeongDEV <yeong7098@naver.com>
2024-12-28 16:14:47 +0900

    ADD : FlatList를 사용한 주문 목록 띄우기

--- FoodDeliveryApp/src/store/reducer.ts
+++ FoodDeliveryApp/src/store/reducer.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from "@reduxjs/toolkit";
 
 import UserSlice from '../slices/user';
+import orderSlice from "../slices/order.ts";
 
 
 const rootReducer = combineReducers({
     user : UserSlice.reducer,
+    order : orderSlice.reducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
